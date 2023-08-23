96fa86c1f37a34caa39ff2062e9c970a4f45ab44
PAUSCODE <143001334+PAUSCODE@users.noreply.github.com>
2023-08-23 09:31:03 -0500

    Add files via upload

---
+++ FirstCodes/AdditionSubstractionExample.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package additionsubstractionexample;
+
+public class AdditionSubstractionExample {
+
+    public static void main(String[] args) {
+        int numeroZebras = 10;
+        int numeroJir = 3;
+        int totalenZOO = numeroZebras + numeroJir;
+        
+        System.out.println (totalenZOO);
+        int numeroCambio = numeroZebras - 2;
+        System.out.println (numeroCambio);
+    }
+    
+}
