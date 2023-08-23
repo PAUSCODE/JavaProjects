638ed42fd3084a9226402d6f944c808c19c2c075
PAUSCODE <143001334+PAUSCODE@users.noreply.github.com>
2023-08-23 09:26:13 -0500

    Add files via upload

---
+++ FirstCodes/Class_Tree.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package class_tree;
+
+/**
+ *
+ * @author Estudiantes
+ */
+public class Class_Tree {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hey there, I'm Paula and im learning to code");
+        System.out.println("Im going to plant a beutiful tree today!");
+        System.out.println("   **   ");
+        System.out.println("  *****  ");
+        System.out.println("*********");
+        System.out.println(" *******  ");
+        System.out.println("  *****  ");
+        System.out.println("    *  ");
+        System.out.println("    *  ");
+        System.out.println("    *  ");
+        System.out.println("    *  ");
+    }
+    
+}
