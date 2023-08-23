a007a54718531ee83bf4e68310ab153a57c9debd
PAUSCODE <143001334+PAUSCODE@users.noreply.github.com>
2023-08-23 09:44:47 -0500

    Add files via upload

---
+++ FirstCodes/ConcatenationExample.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package concatenationexample;
+
+ class ConcatenationExample {
+
+    public static void main(String[] args) {
+        int animal = 3;
+        String especie = "elefante";
+        String descripcionZOO = "el zoo tiene " + animal + " " + especie+ "s";
+        System.out.println(descripcionZOO);
+    }
+    
+}
