54cd285211bbfe796b8b18b669dff84e2a5c664f
PAUSCODE <143001334+PAUSCODE@users.noreply.github.com>
2023-08-23 09:34:14 -0500

    Add files via upload

---
+++ FirstCodes/CharExample.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package charexample;
+
+public class CharExample {
+
+    public static void main(String[] args) {
+       char expectedGrade = 'C';
+       System.out.println(expectedGrade);
+    }
+    
+}
