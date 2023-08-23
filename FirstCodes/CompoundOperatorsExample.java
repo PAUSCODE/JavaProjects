6f9a562c013dbaac93d69099443ffacb09acc20b
PAUSCODE <143001334+PAUSCODE@users.noreply.github.com>
2023-08-23 09:38:22 -0500

    Add files via upload

---
+++ FirstCodes/CompoundOperatorsExample.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package compoundoperatorsexample;
+
+public class CompoundOperatorsExample {
+
+    public static void main(String[] args) {
+       int numGal = 40;
+       numGal -= 12;
+       System.out.println(numGal);
+       numGal /= 10;
+       System.out.println(numGal);
+    }
+    
+}
