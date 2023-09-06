e2666d9ce3d9b67db70268cdfa40a65a7e62dc2e
PAUSCODE <143001334+PAUSCODE@users.noreply.github.com>
2023-09-06 09:06:56 -0500

    Add files via upload

---
+++ FirstCodes/variables1.java
@@ -0,0 +1,12 @@
+
+package variables1.java;
+
+
+public class variables1 {
+	public static void main(String[] args) {
+        String name = "James Gosling";
+        int yearCreated = 1995;      //The int data type allows values between -2,147,483,648 and 2,147,483,647, inclusive.
+        System.out.println(name);
+        System.out.println(yearCreated);
+	}
+}
