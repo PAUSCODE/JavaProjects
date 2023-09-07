8893175241c83745bfb3460ebe08afc5531624e1
PAUSCODE <143001334+PAUSCODE@users.noreply.github.com>
2023-09-07 09:34:49 -0500

    Add files via upload

---
+++ Bank.java
@@ -0,0 +1,21 @@
+package bank;
+
+public class Bank{
+  private CheckingAccount accountOne;
+  private CheckingAccount accountTwo;
+  private CheckingAccount accountThree;
+
+  public Bank(){
+    accountOne = new CheckingAccount("Zeus", 100);
+    accountTwo = new CheckingAccount("Hades", 200);
+    accountThree = new CheckingAccount("Afrodita", 800);
+  }
+
+  public static void main(String[] args){
+    Bank bankOfGods = new Bank();
+    System.out.println(bankOfGods.accountOne.name);
+    System.out.println(bankOfGods.accountOne.balance);
+    System.out.println(bankOfGods.accountThree.name);
+    System.out.println(bankOfGods.accountThree.balance);
+  }
+}
