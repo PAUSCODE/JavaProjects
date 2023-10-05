75486b4595b89d58dd5052edf74fb35b39b05539
PAUSCODE <143001334+PAUSCODE@users.noreply.github.com>
2023-10-05 09:45:41 -0500

    Add files via upload

---
+++ SEGUNDO CORTE/Herencia/TestHerencia.java
@@ -0,0 +1,12 @@
+package test;
+
+import herencia.*;
+
+public class TestHerencia{
+        public static void main (String [] args) {
+            Persona per1 = new Persona ("Harry Styles" ,'H',27, "Santa Barbara");
+            
+            System.out.println(per1);
+        }
+}
+
