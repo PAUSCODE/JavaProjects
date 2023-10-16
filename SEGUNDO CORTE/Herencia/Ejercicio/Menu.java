f7f30101e0b2dcb13a56517e80c44febc27e9964
PAUSCODE <143001334+PAUSCODE@users.noreply.github.com>
2023-10-16 13:50:09 -0500

    Update Menu.java

--- SEGUNDO CORTE/Herencia/Ejercicio/Menu.java
+++ SEGUNDO CORTE/Herencia/Ejercicio/Menu.java
@@ -11,8 +11,10 @@ public class Menu {
 	
 	public void menu (){
 		Scanner s=new Scanner(System.in);
+		System.out.println("Elija una opción: ");
 		System.out.println("1. Area Circulo");
 		System.out.println("2. Volumen Esfera");
+		System.out.println("3. Volumen Cilindro");
 		System.out.println("Cualquier numero salir");
 		int opcion=s.nextInt();
 		if (opcion == 1){
@@ -20,6 +22,9 @@ public class Menu {
 		} else{
 			if (opcion ==2){
 				objVEs.mostrar(objVEs.cvesf(), "El volumen de la esfera es ");
+			}else{
+			if (opcion ==3){
+				objVCil.mostrar(objVCil.calcularVCil(), "El volumen de la esfera es ");
 			}else{
 				System.out.println("Gracias por usar el software");
 			}
