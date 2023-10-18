55606eb35cd3ace46671e8c49e6c3051d98744f5
PAUSCODE <143001334+PAUSCODE@users.noreply.github.com>
2023-10-18 09:51:02 -0500

    Update TestHerencia.java

--- SEGUNDO CORTE/Herencia/TestHerencia.java
+++ SEGUNDO CORTE/Herencia/TestHerencia.java
@@ -7,12 +7,12 @@ public class TestHerencia {
     public static void main(String[] args) {
         
         //Crear empleados 
-        Employee empleado1 = new Employee("Harry", 3000);
+        Employee empleado1 = new Employee(10000, "Harry");
         System.out.println(empleado1);
-        Employee empleado2 = new Employee("Mafe", 4000);
+        Employee empleado2 = new Employee(10100, "Mafe");
         System.out.println(empleado2);
         
-        Employee empleado3 = new Employee("Juanito", 5000);
+        Employee empleado3 = new Employee(10000, "Juanito");
         System.out.println(empleado3);
         
          
