2591ed153b76304d3ee8251afc3b9c124cd893aa
PAUSCODE <143001334+PAUSCODE@users.noreply.github.com>
2023-10-18 09:40:54 -0500

    Update TestHerencia.java

--- SEGUNDO CORTE/Herencia/TestHerencia.java
+++ SEGUNDO CORTE/Herencia/TestHerencia.java
@@ -2,11 +2,22 @@ package test;
 
 import herencia.*;
 
-public class TestHerencia{
-        public static void main (String [] args) {
-            Persona per1 = new Persona ("Harry Styles" ,'H',27, "Santa Barbara");
-            
-            System.out.println(per1);
-        }
+import java.util.Date;
+public class TestHerencia {
+    public static void main(String[] args) {
+        
+        //Crear empleados 
+        Employee empleado1 = new Employee("Alex", 3000);
+        System.out.println(empleado1);
+        Employee empleado2 = new Employee("Reol", 4000);
+        System.out.println(empleado2);
+        
+        Employee empleado3 = new Employee("Juan", 5000);
+        System.out.println(empleado3);
+        
+        //Crear Clientes 
+        var fecha = new Date(); //Obtener la fecha de hoy 
+        Client cliente1 = new Client(fecha, true, "Suny", 'F', 18,"Santo tomas");
+        System.out.println(cliente1);        
+    }
 }
-
